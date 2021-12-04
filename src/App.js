c4501b21abd74e0847a24a7a82743bcabd192cc9
unknown <1851161809@e.tlu.edu.vn>
2021-12-04 19:57:19 +0700

    design trello frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,116 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="trello">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+        <header>Brainstore</header>
+        <ul>
+          <li>
+            <img src="http://st.imageinstant.net/data/comics/200/my-home-hero.jpg"
+                 alt="trello01-alt-img"/>
+            Title: Hayhaha
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstore</header>
+        <ul>
+          <li>
+            <img src="http://st.imageinstant.net/data/comics/200/my-home-hero.jpg"
+                 alt="trello01-alt-img"/>
+            Title: Hayhaha
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+        <header>Brainstore</header>
+        <ul>
+          <li>
+            <img src="http://st.imageinstant.net/data/comics/200/my-home-hero.jpg"
+                 alt="trello01-alt-img"/>
+            Title: Hayhaha
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+        <header>Brainstore</header>
+        <ul>
+          <li>
+            <img src="http://st.imageinstant.net/data/comics/200/my-home-hero.jpg"
+                 alt="trello01-alt-img"/>
+            Title: Hayhaha
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+        <header>Brainstore</header>
+        <ul>
+          <li>
+            <img src="http://st.imageinstant.net/data/comics/200/my-home-hero.jpg"
+                 alt="trello01-alt-img"/>
+            Title: Hayhaha
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+        <header>Brainstore</header>
+        <ul>
+          <li>
+            <img src="http://st.imageinstant.net/data/comics/200/my-home-hero.jpg"
+                 alt="trello01-alt-img"/>
+            Title: Hayhaha
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
